29b5824ed4c6c83ed3ad9a5b0ef58ea3e72e5818
Ozan ERCAN <ozanercann@gmail.com>
2025-01-26 10:44:35 +0300

    add test for more edge cases

--- src/test/java/org/ozanercan/AppTest.java
+++ src/test/java/org/ozanercan/AppTest.java
@@ -270,4 +270,150 @@ public class AppTest {
         assertEquals(2, summary.get(4).getAwayScore());
     }
 
+    @Test
+    public void testStartingFinishingAndRestartingSameMatch() {
+        scoreboard.startMatch("England", "Scotland");
+        scoreboard.updateScore("England", "Scotland", 1, 0);
+        scoreboard.finishMatch("England", "Scotland");
+
+        List<Match> summaryAfterFinish = scoreboard.getSummary();
+        assertTrue(summaryAfterFinish.stream().noneMatch(match ->
+                match.getHomeTeam().equals("England") && match.getAwayTeam().equals("Scotland")
+        ));
+
+        scoreboard.startMatch("England", "Scotland");
+        scoreboard.updateScore("England", "Scotland", 2, 2);
+
+        List<Match> summaryAfterRestart = scoreboard.getSummary();
+        assertTrue(summaryAfterRestart.stream().anyMatch(match ->
+                match.getHomeTeam().equals("England") && match.getAwayTeam().equals("Scotland") &&
+                        match.getHomeScore() == 2 && match.getAwayScore() == 2
+        ));
+
+        scoreboard.finishMatch("England", "Scotland");
+
+        List<Match> summaryAfterSecondFinish = scoreboard.getSummary();
+        assertTrue(summaryAfterSecondFinish.stream().noneMatch(match ->
+                match.getHomeTeam().equals("England") && match.getAwayTeam().equals("Scotland")
+        ));
+    }
+
+    @Test
+    public void testFinishAllMatchesWithoutUpdateScore() {
+        scoreboard.startMatch("England", "Scotland");
+        scoreboard.finishMatch("England", "Scotland");
+
+        List<Match> summary = scoreboard.getSummary();
+        assertTrue(summary.isEmpty());
+    }
+
+    @Test
+    public void testLargeNumberOfMatches() {
+        for (int i = 1; i <= 10000; i++) {
+            scoreboard.startMatch("Team" + i, "Opponent" + i);
+            scoreboard.updateScore("Team" + i, "Opponent" + i, i, i + 1);
+        }
+
+        List<Match> summary = scoreboard.getSummary();
+        assertEquals(10000, summary.size());
+        assertEquals("Team10000", summary.get(0).getHomeTeam());
+        assertEquals("Opponent10000", summary.get(0).getAwayTeam());
+
+        for (int i = 1; i <= 10000; i++) {
+            scoreboard.finishMatch("Team" + i, "Opponent" + i);
+        }
+        List<Match> summaryAfterFinish = scoreboard.getSummary();
+        assertTrue(summaryAfterFinish.isEmpty());
+    }
+
+    @Test
+    public void testRemoveMiddleMatch() {
+        scoreboard.startMatch("Atletico Madrid", "Juventus");
+        scoreboard.startMatch("Tottenham Hotspur", "Inter Milan");
+        scoreboard.startMatch("Napoli", "RB Leipzig");
+
+        scoreboard.updateScore("Atletico Madrid", "Juventus", 2, 1);
+        scoreboard.updateScore("Tottenham Hotspur", "Inter Milan", 3, 4);
+        scoreboard.updateScore("Napoli", "RB Leipzig", 1, 1);
+
+        scoreboard.finishMatch("Tottenham Hotspur", "Inter Milan");
+
+        List<Match> summary = scoreboard.getSummary();
+        assertEquals(2, summary.size());
+        assertEquals("Atletico Madrid", summary.get(0).getHomeTeam());
+        assertEquals("Napoli", summary.get(1).getHomeTeam());
+    }
+
+    @Test
+    public void testDynamicSortingInSummarySameScores() {
+        scoreboard.startMatch("Borussia Dortmund", "Sevilla FC");
+        scoreboard.startMatch("AS Roma", "Benfica");
+        scoreboard.startMatch("Napoli", "RB Leipzig");
+        scoreboard.startMatch("Lazio", "Porto");
+
+        scoreboard.updateScore("Borussia Dortmund", "Sevilla FC", 2, 3);
+        scoreboard.updateScore("AS Roma", "Benfica", 1, 1);
+        scoreboard.updateScore("Napoli", "RB Leipzig",1,1);
+        scoreboard.updateScore("Lazio", "Porto", 4, 2);
+
+        List<Match> summary = scoreboard.getSummary();
+
+        assertEquals("Lazio", summary.get(0).getHomeTeam());
+        assertEquals("Borussia Dortmund", summary.get(1).getHomeTeam());
+        assertEquals("Napoli", summary.get(2).getHomeTeam());
+        assertEquals("AS Roma", summary.get(3).getHomeTeam());
+    }
+
+    @Test
+    public void testUpdateScoreOfStartedMatchWithSwitchedTeamNames() {
+        scoreboard.startMatch("Chelsea", "PSG");
+        assertThrows(MatchNotFoundException.class,
+                () -> scoreboard.updateScore("PSG", "Chelsea",1,1));
+    }
+
+    @Test
+    public void testStartMatchesWithSameTeamInOneRole() {
+        scoreboard.startMatch("Chelsea", "Barcelona");
+        assertThrows(DuplicateMatchFoundException.class,
+                () -> scoreboard.startMatch("Chelsea", "Real Madrid"));
+        assertThrows(DuplicateMatchFoundException.class,
+                () -> scoreboard.startMatch( "Real Madrid", "Chelsea"));
+        assertThrows(DuplicateMatchFoundException.class,
+                () -> scoreboard.startMatch("Barcelona", "Real Madrid"));
+        assertThrows(DuplicateMatchFoundException.class,
+                () -> scoreboard.startMatch( "Real Madrid", "Barcelona"));
+    }
+
+    @Test
+    public void testUpdateScoreAndFinishMatchWithNullOrEmptyTeamNames() {
+        scoreboard.startMatch("Chelsea", "PSG");
+        assertThrows(IllegalArgumentException.class,
+                () -> scoreboard.updateScore(null, "PSG", 1, 1));
+        assertThrows(IllegalArgumentException.class,
+                () -> scoreboard.updateScore("Chelsea", null, 1, 1));
+        assertThrows(IllegalArgumentException.class,
+                () -> scoreboard.updateScore(null, null, 1, 1));
+        assertThrows(IllegalArgumentException.class,
+                () -> scoreboard.finishMatch(null, "PSG"));
+        assertThrows(IllegalArgumentException.class,
+                () -> scoreboard.finishMatch("Chelsea", null));
+        assertThrows(IllegalArgumentException.class,
+                () -> scoreboard.finishMatch(null, null));
+        assertThrows(IllegalArgumentException.class,
+                () -> scoreboard.updateScore("", "PSG", 1, 1));
+        assertThrows(IllegalArgumentException.class,
+                () -> scoreboard.updateScore("Chelsea", "", 1, 1));
+        assertThrows(IllegalArgumentException.class,
+                () -> scoreboard.updateScore("", "", 1, 1));
+        assertThrows(IllegalArgumentException.class,
+                () -> scoreboard.finishMatch("", "PSG"));
+        assertThrows(IllegalArgumentException.class,
+                () -> scoreboard.finishMatch("Chelsea", ""));
+        assertThrows(IllegalArgumentException.class,
+                () -> scoreboard.finishMatch("", ""));
+    }
+
+
+
+
 }
